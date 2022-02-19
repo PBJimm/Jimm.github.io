461d138ce0bcd10bc0509b43a08421109ba378be
PBJimm <20ftt1813@student.pb.edu.bn>
2022-02-19 10:22:05 +0800

    Pindah repo

---
+++ awepas2g1/function.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            
+            if(isset($_POST['submit'])){
+                
+                echo $_POST['studentName'];
+            }
+        ?>
+    </body>
+</html>
